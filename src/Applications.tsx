1a12764fddf4e33c9eb85621ee1be023ee142596
Will Njoroge <wnjoroge@deloitte.co.uk>
2025-01-17 13:25:57 +0000

    implement fetch of applications and add loading state to Button component

--- src/Applications.tsx
+++ src/Applications.tsx
@@ -1,15 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SingleApplication from "./SingleApplication";
-import { getSingleApplicationFixture } from "./__fixtures__/applications.fixture";
 import styles from "./Applications.module.css";
+import { Button } from "./ui/Button/Button";
 
 const Applications = () => {
-  const applications = getSingleApplicationFixture;
+  const [applications, setApplications] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchApplications = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/applications?_page=${page}&_limit=5`
+      );
+      const data = await response.json();
+
+      const linkHeader = response.headers.get("Link");
+      const hasNextPage = linkHeader?.includes('rel="next"') ? true : false;
+      setHasMore(hasNextPage);
+
+      setApplications((prevApplications) => [...prevApplications, ...data]);
+    } catch (error) {
+      console.error("Error", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const loadMoreApplications = () => {
+    if (!isLoading && hasMore) {
+      setPage(page + 1);
+    }
+  };
+
+  useEffect(() => {
+    fetchApplications();
+  }, [page]);
 
   return (
-    <div className={styles.Applications}>
-      <SingleApplication application={applications[0]} />
-    </div>
+    <>
+      <div className={styles.Applications}>
+        {applications.map((application) => (
+          <SingleApplication key={application.id} application={application} />
+        ))}
+
+        {hasMore && (
+          <Button
+            className={styles.button}
+            isLoading={isLoading}
+            onClick={loadMoreApplications}
+          />
+        )}
+
+        {!hasMore && !isLoading && (
+          <p>All {applications.length} applications have been loaded</p>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import cn from "classnames";
 import styles from "./Button.module.css";
 
-export const Button = ({ className, ...buttonProps }) => {
-  return <button className={cn(styles.button, className)} {...buttonProps} />;
+export const Button = ({ className, isLoading, ...buttonProps }) => {
+  return (
+    <button
+      className={cn(styles.button, className)}
+      disabled={isLoading}
+      {...buttonProps}
+    >
+      {isLoading ? "Loading..." : "Load more"}
+    </button>
+  );
 };
